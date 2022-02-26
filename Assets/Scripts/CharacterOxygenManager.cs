61f9a5e87b04f303feff7af729c6927b96741aff
Mikolaj Zurawski <mikzur4@gmail.com>
2022-02-26 13:04:21 +0100

    genericise PlayerOxygenManager, add events on death and hp change

--- Assets/Scripts/CharacterOxygenManager.cs
+++ Assets/Scripts/CharacterOxygenManager.cs
@@ -2,8 +2,9 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class PlayerOxygenManager : MonoBehaviour
+public class CharacterOxygenManager : MonoBehaviour
 {
 
     private static float _speedOfLoosingOxygen = 1f;
@@ -12,25 +13,41 @@ public class PlayerOxygenManager : MonoBehaviour
     private static float _maximumOxygenLoss = 3f;
     private static float _radiusOfOxygenLoss = 9f;
 
+    public UnityEvent m_EventDeath;
+    public UnityEvent<float> m_EventChangeOxygenState;
+
     [HideInInspector]
     public HashSet<Fire> FiresWhichAffectPlayer { get; private set; }
 
     [SerializeField]
     private float _maxOxygen;
-
     private float _currentOxygen;
+
+    private bool isAlive;
 
     // Start is called before the first frame update
     void Start()
     {
+        if (_maxOxygen > 0) 
+        {
+            isAlive = true;
+        }
+
         FiresWhichAffectPlayer = new HashSet<Fire>();
         _currentOxygen = _maxOxygen;
+
+        m_EventDeath = new UnityEvent();
+        m_EventChangeOxygenState = new UnityEvent<float>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(FiresWhichAffectPlayer.Count == 0)
+        if (!isAlive) {
+            return;
+        }
+
+        if(FiresWhichAffectPlayer.Count == 0 && _currentOxygen <= 0f)
         {
             _currentOxygen = _currentOxygen + _speedOfRegainingOxygen * Time.deltaTime > _maxOxygen
                 ? _maxOxygen
@@ -42,7 +59,15 @@ public class PlayerOxygenManager : MonoBehaviour
             _currentOxygen -= Mathf.Clamp(_speedOfLoosingOxygen * (_radiusOfOxygenLoss - Vector3.Distance(transform.position, fire.transform.position) * Time.deltaTime),
                                           0f,
                                           _maximumOxygenLoss * Time.deltaTime);
-            Debug.LogFormat("Oxygen of the player {0} out of {1} after loosing it", _currentOxygen, _maxOxygen);
+            Debug.LogFormat("Oxygen of the character {0}: {1} out of {2} after loosing it", gameObject.name, _currentOxygen, _maxOxygen);
+        }
+
+        m_EventChangeOxygenState.Invoke(_currentOxygen);
+        
+        if (_currentOxygen <= 0f) 
+        {
+            m_EventDeath.Invoke();
+            isAlive = false;
         }
     }
 
@@ -56,9 +81,9 @@ public class PlayerOxygenManager : MonoBehaviour
         }
         if (!FiresWhichAffectPlayer.Add(fire))
         {
-            Debug.LogErrorFormat("Fire {0} added twice to the zone of fires affected by player", other.gameObject.name);
+            Debug.LogErrorFormat("Fire {0} added twice to the zone of fires which affect character", other.gameObject.name);
         }
-        Debug.Log("Fire affected a Player");
+        Debug.Log("Fire affected a Character");
     }
 
     private void OnTriggerExit(Collider other)
@@ -70,10 +95,10 @@ public class PlayerOxygenManager : MonoBehaviour
         }
         if (!FiresWhichAffectPlayer.Remove(fire))
         {
-            Debug.LogErrorFormat("Fire has {0} left the zone of fires affected by player, but it was never inside", other.gameObject.name);
+            Debug.LogErrorFormat("Fire has {0} left the zone of fires affected by character, but it was never inside", other.gameObject.name);
         }
         fire.FireExtinguished -= c_onDestroy;
-        Debug.Log("Fire removed from fires by which player is affected");
+        Debug.Log("Fire removed from fires by which character is affected");
     }
 
     private void c_onDestroy(object sender, EventArgs e)

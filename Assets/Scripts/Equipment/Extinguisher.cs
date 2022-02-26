693900a3291b32bcb260007e49da8f3e90b317b7
Pojemnik <andriej194@gmail.com>
2022-02-26 18:57:26 +0100

    Add fire extinguisher fuel and fuel bar

--- Assets/Scripts/Equipment/Extinguisher.cs
+++ Assets/Scripts/Equipment/Extinguisher.cs
@@ -4,17 +4,36 @@ using UnityEngine;
 
 public class Extinguisher : MonoBehaviour
 {
+    [Header("References")]
     [SerializeField]
     private ExtingiushingZone _extinguishingZone;
 
+    [Header("Fuel")]
+    [SerializeField]
+    private float _maxFuel;
+    [SerializeField]
+    private float _fuelUsage;
+
+    public UnityEngine.Events.UnityEvent<float> FuelPercentageUpdatedEvent;
+
     private bool _extinguishing = false;
+    private float _currentFuel;
+
 
     private void Start()
     {
-        if(_extinguishingZone == null)
+        if (_extinguishingZone == null)
         {
             throw new System.NullReferenceException("No extinguishing zone in fire extinguisher");
         }
+        _currentFuel = _maxFuel;
+        FuelPercentageUpdatedEvent.Invoke(100);
+    }
+
+    public void RefillFuel()
+    {
+        _currentFuel = _maxFuel;
+        FuelPercentageUpdatedEvent.Invoke(100);
     }
 
     public void StartExtinguishing()
@@ -29,15 +48,26 @@ public class Extinguisher : MonoBehaviour
 
     void Update()
     {
-        if (_extinguishing)
+        if (_extinguishing && _currentFuel > 0)
         {
             Extinguish();
+            UpdateFuelLevel();
         }
+    }
+
+    private void UpdateFuelLevel()
+    {
+        _currentFuel -= _fuelUsage * Time.deltaTime;
+        if (_currentFuel < 0)
+        {
+            _currentFuel = 0;
+        }
+        FuelPercentageUpdatedEvent.Invoke((_currentFuel / _maxFuel) * 100f);
     }
 
     public void Extinguish()
     {
-        foreach(Fire fire in _extinguishingZone.FiresInZone)
+        foreach (Fire fire in _extinguishingZone.FiresInZone)
         {
             fire.Extinguish();
         }

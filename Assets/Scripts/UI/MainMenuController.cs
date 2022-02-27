b75a0e7d8488a742f212ddb0ea5978da21e66e1d
Pojemnik <andriej194@gmail.com>
2022-02-27 01:40:24 +0100

    Add main menu functionality

--- Assets/Scripts/UI/MainMenuController.cs
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -5,7 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
+    [Header("References")]
+    [SerializeField]
+    UnityEngine.UI.Button _quitButton;
+
     private void Awake()
     {
+        _quitButton.onClick.AddListener(() => Application.Quit());
     }
 }
